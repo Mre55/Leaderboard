8849bd052166984ba06988709d0ed94d3121f69f
mre55 <misganawmihreteab>
2021-12-15 01:11:23 +0300

    fix eslint error

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -13,8 +12,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: 'main.js',
-    publicPath: "/Leaderboard/",
+    publicPath: '/Leaderboard/',
   },
   module: {
     rules: [
